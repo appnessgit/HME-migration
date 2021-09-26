4711388ad8feb2872c34e5187701c1aca610c7ce
Omer Hassan <o.hassan@app-ness.com>
2021-09-26 13:28:21 +0200

    purchace + sales changes

--- hme_sale/models/sale.py
+++ hme_sale/models/sale.py
@@ -3,7 +3,8 @@ from odoo import models, fields, api
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
-
+    
+    name = fields.Char(readonly = False)
     brand_id = fields.Many2one('res.partner', string="Brand", domain=[('is_brand', '=', True)])
     batch_num = fields.Many2one('stock.production.lot', 'Batch Number')
     expiry_date = fields.Datetime('Expiry Date')
@@ -13,4 +14,9 @@ class SaleOrderLine(models.Model):
     def _onchange_brand_id(self):
         return {'domain': {'product_id': [('brand_id', '=', self.brand_id.id)]}}
 
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+    
+    name = fields.Char(readonly = False)
 
